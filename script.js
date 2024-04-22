37f3d369d006765bd974db8687a466f73e8e642c
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-04-22 18:41:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 window.onload = function() {
     let slides = document.querySelectorAll('.slideshow .slide');
     let currentSlide = 0;
+    let isPaused = false;
 
     function nextSlide() {
         slides[currentSlide].classList.remove('active');
-        currentSlide = (currentSlide+1)%slides.length;
+        currentSlide = (currentSlide + 1) % slides.length;
         slides[currentSlide].classList.add('active');
+        if (currentSlide === Math.floor(slides.length / 2)) {
+            isPaused = true; // Pause the slideshow when an image is in the center
+            setTimeout(() => {
+                isPaused = false; // Continue after 1.5 seconds
+            }, 1500);
+        }
     }
 
     slides[currentSlide].classList.add('active');
-    setInterval(nextSlide, 1500); // Change slide every 1.5 seconds
+    setInterval(() => {
+        if (!isPaused) {
+            nextSlide();
+        }
+    }, 1500); // Change slide every 1.5 seconds
 }
