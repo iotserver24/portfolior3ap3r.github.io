4852a92486dc07c83eef6263043a54b17ed0c053
R3AP3Redit <147928812+iotserver24@users.noreply.github.com>
2024-04-22 18:17:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,16 @@ window.onload = function() {
 
     function nextSlide() {
         slides[currentSlide].classList.remove('active');
-        currentSlide = (currentSlide+1)%slides.length;
+        currentSlide = (currentSlide + 1) % slides.length;
         slides[currentSlide].classList.add('active');
     }
 
-    slides[currentSlide].classList.add('active');
-    setInterval(nextSlide, 1500); // Change slide every 1.5 seconds
+    // Initial activation of all slides
+    slides.forEach((slide, index) => {
+        setTimeout(() => {
+            slide.classList.add('active');
+        }, 1500 * index); // Delay activation based on index
+    });
+
+    setInterval(nextSlide, 1500 * slides.length); // Change slide every (1.5 * number of slides) seconds
 }
